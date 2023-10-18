b1b327b5bfb13119566b66f90b550b2e1cd8dde4
Ariful Islam <ariful.rony10@gmail.com>
2023-10-18 23:05:02 +0600

    install mongoose and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // require dot env file
+      isGlobal: true,
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
